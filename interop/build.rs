e35dd10ef0504e754e906204b8986b4366236cc1
Natan <natanvnascimento@gmail.com>
2025-12-26 20:37:38 -0300

    feat: rust dns resolver

---
+++ interop/build.rs
@@ -0,0 +1,18 @@
+extern crate cbindgen;
+
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let output_file = PathBuf::from(&crate_dir).join("http_native.h");
+
+    cbindgen::Builder::new()
+        .with_crate(crate_dir)
+        .with_language(cbindgen::Language::C)
+        .with_include_guard("HTTP_NATIVE_H")
+        .with_cpp_compat(true)
+        .generate()
+        .expect("Unable to generate bindings")
+        .write_to_file(output_file);
+}
