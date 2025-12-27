b2d7ffa04d8df3bb422577d097173ea5fe371efa
Natan <natanvnascimento@gmail.com>
2025-12-26 22:04:20 -0300

    feat: native http client impl

--- lib/src/commonMain/kotlin/me/devnatan/dockerkt/native/NativeHttpClient.kt
+++ lib/src/commonMain/kotlin/me/devnatan/dockerkt/native/NativeHttpClient.kt
@@ -1,12 +1,28 @@
 package me.devnatan.dockerkt.native
 
 import io.ktor.client.engine.*
+import io.ktor.client.request.HttpRequestData
+import io.ktor.client.request.HttpResponseData
+import io.ktor.utils.io.InternalAPI
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlin.coroutines.CoroutineContext
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
 
 public class NativeHttpEngineConfig : HttpClientEngineConfig() {
     public var customDnsServers: List<String> = emptyList()
     public var unixSocketPath: String? = null
+    public var requestTimeout: Duration = 5.seconds
 }
 
-public expect abstract class NativeHttpEngine(config: NativeHttpEngineConfig) : HttpClientEngine
+public expect class NativeHttpEngine(config: NativeHttpEngineConfig) : HttpClientEngine {
+    override val dispatcher: CoroutineDispatcher
+    override val config: HttpClientEngineConfig
+
+    @InternalAPI
+    override suspend fun execute(data: HttpRequestData): HttpResponseData
+    override val coroutineContext: CoroutineContext
+    override fun close()
+}
 
 public expect val NativeHttpEngineFactory: HttpClientEngineFactory<NativeHttpEngineConfig>
