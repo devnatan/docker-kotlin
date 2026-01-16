45230f854259bfeaff0c9c09e4aea78139d9f154
Natan <natanvnascimento@gmail.com>
2026-01-15 21:20:02 -0300

    Update Image model

--- src/commonMain/kotlin/me/devnatan/dockerkt/models/image/Image.kt
+++ src/commonMain/kotlin/me/devnatan/dockerkt/models/image/Image.kt
@@ -5,36 +5,48 @@ package me.devnatan.dockerkt.models.image
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import me.devnatan.dockerkt.models.GraphDriverData
-import me.devnatan.dockerkt.models.container.ContainerConfig
+import me.devnatan.dockerkt.models.container.VolumesSerializer
 import kotlin.time.ExperimentalTime
 import kotlin.time.Instant
 
 @Serializable
 public data class Image(
     @SerialName("Id") public val id: String,
-    @SerialName("Parent") public val parent: String,
-    @SerialName("Comment") public val comment: String,
-    @SerialName("Created") public val created: String,
-    @SerialName("Container") public val container: String,
-    @SerialName("DockerVersion") public val dokerVersion: String,
-    @SerialName("Author") public val author: String,
-    @SerialName("Architecture") public val arch: String,
+    @SerialName("RepoTags") public val repositoryTags: List<String> = emptyList(),
+    @SerialName("RepoDigests") public val repositoryDigests: List<String> = emptyList(),
+    @SerialName("Comment") public val comment: String? = null,
+    @SerialName("Created") public val created: String? = null,
+    @SerialName("Author") public val author: String? = null,
+    @SerialName("Config") public val config: ImageConfig,
+    @SerialName("Architecture") public val architecture: String? = null,
+    @SerialName("Variant") public val variant: String? = null,
     @SerialName("Os") public val os: String,
+    @SerialName("OsVersion") public val osVersion: String? = null,
     @SerialName("Size") public val size: Long,
-    @SerialName("VirtualSize") public val virtualSize: Long,
     @SerialName("GraphDriver") public val graphDriver: GraphDriverData,
     @SerialName("RootFS") public val rootFS: ImageRootFs,
-    @SerialName("RepoTags") public val repositoryTags: List<String>? = null,
-    @SerialName("RepoDigests") public val repositoryDigests: List<String>? = null,
-    @SerialName("ContainerConfig") public val containerConfig: ContainerConfig? = null,
-    @SerialName("Config") public val config: ContainerConfig? = null,
-    @SerialName("OsVersion") public val osVersion: String? = null,
     @SerialName("Metadata") public val metadata: ImageMetadata? = null,
 )
 
+@Serializable
+public data class ImageConfig(
+    @SerialName("User") public val user: String,
+    @SerialName("Env") public val env: List<String> = emptyList(),
+    @SerialName("Cmd") public val command: List<String>? = null,
+    @SerialName("Volumes") public val volumes:
+    @Serializable(with = VolumesSerializer::class)
+    List<String>? = emptyList(),
+    @SerialName("WorkingDir") public val workingDir: String,
+    @SerialName("Entrypoint") public val entrypoint: List<String>? = null,
+    @SerialName("OnBuild") public val onBuild: List<String>? = null,
+    @SerialName("Labels") public val labels: Map<String, String> = emptyMap(),
+    @SerialName("StopSignal") public val stopSignal: String? = null,
+    @SerialName("Shell") public val shell: List<String>? = null,
+)
+
 @Suppress("EXTENSION_SHADOWED_BY_MEMBER")
-public val Image.created: Instant
-    get() = Instant.parse(created)
+public val Image.created: Instant?
+    get() = created?.let(Instant::parse)
 
 @Serializable
 public data class ImageMetadata(
