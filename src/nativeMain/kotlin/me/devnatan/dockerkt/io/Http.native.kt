0ee1f8a29352f126545f0700beeab4386b0f454c
Natan <natanvnascimento@gmail.com>
2026-04-19 22:39:45 -0300

    fix: set native http client request timeout to infinite

--- src/nativeMain/kotlin/me/devnatan/dockerkt/io/Http.native.kt
+++ src/nativeMain/kotlin/me/devnatan/dockerkt/io/Http.native.kt
@@ -10,6 +10,11 @@ import me.devnatan.dockerkt.DockerClient
 internal actual val defaultHttpClientEngine: HttpClientEngineFactory<*>? get() = CIO
 
 internal actual fun <T : HttpClientEngineConfig> HttpClientConfig<out T>.configureHttpClient(client: DockerClient) {
+    engine {
+        require(this is io.ktor.client.engine.cio.CIOEngineConfig) { "Only CIO engine is supported for now" }
+        // disable request timeout so long-running calls (image pulls, log streams) aren't killed
+        requestTimeout = 0
+    }
     defaultRequest {
         val socketPath = client.config.socketPath
         if (isUnixSocket(socketPath)) {
